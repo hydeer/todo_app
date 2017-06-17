3b5f68538ca97ca7808b827496ce9b9bd7790303
hydeer <hy.lavarez.enrica@leadps.org>
2017-06-17 16:42:51 +0000

    New cool update

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var button_content = document.querySelector('h1');
-button_content.addEventListener('click', updateName);
-
-function updateName() {
-    var name = prompt('Enter a new header');
-    button_content.textContext = name;
-}
+alert(".user-name");
